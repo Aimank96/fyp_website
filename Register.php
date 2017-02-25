684f529fb2366ac80b231a8b3488cb27b3dfdd98
aiman <aiman@aiman-X550LD>
2017-02-26 01:57:26 +0800

    Merge origin/master
    Conflicts:
    	index.php

--- Register.php
+++ Register.php
@@ -1,13 +1,19 @@
 <?php
 include 'DBCONFIG.php';
 
-$Username = $_POST['Username'];
-$Password = $_POST['Password'];
+    //$Username = $_POST['Username'];
+   // $Password = $_POST['Password'];
+    //$Email = $_POST['Email'];
+   // $PhoneNumber = $_POST['PhoneNumb'];
+   // $AccBalance = 0;
 
+  $Username ='aa';
+    $Password = 'aa';
+    $Email = '$_POST[Email';
+    $PhoneNumber = 'PhoneNumb';
+    $AccBalance = 0;
 
-
-
-$sql="insert into Users(Username,Password) Values('$Username','$Password') ";
+    $sql="insert into Users(Username,Password,Email,PhoneNumber,AccBalance) Values('$Username','$Password','$Email','$PhoneNumber','$AccBalance') ";
 
 mysqli_query($con,$sql);
 echo 'masuk kew eb';
