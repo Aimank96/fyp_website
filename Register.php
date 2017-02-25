5d1108a31296fd3a56c3c51da4007ebc7a817b8c
aiman <aiman@aiman-X550LD>
2017-02-26 03:04:21 +0800

    Merge origin/master
    Conflicts:
    	index.php

--- Register.php
+++ Register.php
@@ -9,6 +9,8 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     $PhoneNumber = $_POST['PhoneNumb'];
     $AccBalance = 0;
 
+        //   $sql="insert into Users(Username,Password,Email,PhoneNumber,AccBalance) Values('$Username','$Password','$Email','$PhoneNumber','$AccBalance') ";
+                                  //  echo 'Succesfully Register';
     if($Username == '' || $Password == '' || $Email == '' || $PhoneNumber == ''){
         echo 'Please fill all values';
     } else {
@@ -16,15 +18,24 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                                  echo'Invalid email format'; 
                               } else {
-                                     $sql="insert into Users(Username,Password,Email,PhoneNumber,AccBalance) Values('$Username','$Password','$Email','$PhoneNumber','$AccBalance') ";
-                                    echo 'Succesfully Register';
-                              }
+                              $sql = "SELECT * FROM Users WHERE Username='$Username' OR Email='$email' OR PhoneNumber=$PhoneNumber";                                    
+		              $check = mysqli_fetch_array(mysqli_query($con,$sql));
+                              if(isset($check)){
+				echo 'username or email or phone Number already Register';
+			         }else{				      
+                                 $sql="insert into Users(Username,Password,Email,PhoneNumber,AccBalance) Values('$Username','$Password','$Email','$PhoneNumber','$AccBalance') ";
+                                     if(mysqli_query($con,$sql)){
+					echo 'successfully registered';
+				}else{
+                                  
+					echo 'oops! Please try again!';
+				}
+                                 }
     }
     
     
     
 
-mysqli_query($con,$sql);
 
 }
 ?>
