a487863dcedc46b8c7041ee9695803d43f0970f3
aiman <aiman@aiman-X550LD>
2017-02-26 01:26:35 +0800

    Merge origin/master
    Conflicts:
    	index.php

--- Register.php
+++ Register.php
@@ -1,15 +1,14 @@
 <?php
 include 'DBCONFIG.php';
 
-//$Username = $_POST['Username'];
-//$Password = $_POST['Password'];
+$Username = $_POST['Username'];
+$Password = $_POST['Password'];
 
-$Username ='ssss';
-$Password='sssa';
+
+
 
 $sql="insert into Users(Username,Password) Values('$Username','$Password') ";
 
 mysqli_query($con,$sql);
 
-echo "username is $Username password is '$Password'" ;
 ?>
