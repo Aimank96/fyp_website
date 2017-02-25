b4a2c92951d384929c8dbe5194e32a2f38be602f
aiman <aiman@aiman-X550LD>
2017-02-26 02:29:42 +0800

    Merge origin/master
    Conflicts:
    	index.php

--- Register.php
+++ Register.php
@@ -1,16 +1,10 @@
 <?php
 include 'DBCONFIG.php';
 
-    //$Username = $_POST['Username'];
-   // $Password = $_POST['Password'];
-    //$Email = $_POST['Email'];
-   // $PhoneNumber = $_POST['PhoneNumb'];
-   // $AccBalance = 0;
-
-  $Username ='aa';
-    $Password = 'aa';
-    $Email = '$_POST[Email';
-    $PhoneNumber = 'PhoneNumb';
+  $Username = $_POST['Username'];
+    $Password = $_POST['Password'];
+    $Email = $_POST['Email'];
+    $PhoneNumber = $_POST['PhoneNumb'];
     $AccBalance = 0;
 
     $sql="insert into Users(Username,Password,Email,PhoneNumber,AccBalance) Values('$Username','$Password','$Email','$PhoneNumber','$AccBalance') ";
