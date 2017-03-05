1208b9aa1c93e4762ef42590596f6b20db9fbf0f
aiman <aiman@aiman-X550LD>
2017-03-05 16:13:01 +0800

    Merge origin/master
    Conflicts:
    	index.php

--- viewDetails.php
+++ viewDetails.php
@@ -4,12 +4,18 @@ include 'DBCONFIG.php';
 
 $ID=$_POST["ID"];
 
-$sql="select * from Users where ID = '$ID'";
+$sql="select * from Users where ID ='1' ";//'$ID'";
         
-  $res = mysqli_query($con,$sql);
+  $result = mysqli_query($con,$sql);
  
-$result = array();
- 
+  if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row[0]. " - Name: " . $row[1]. " " . $row[]. "<br>";
+    }
+  }
+
+ /**
 while($row = mysqli_fetch_array($res)){
 array_push($result,
 array('ID'=>$row[0],
@@ -21,6 +27,8 @@ array('ID'=>$row[0],
 echo json_encode(array("result"=>$result));
 
 mysqli_close($con);
+  **/
+  
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
