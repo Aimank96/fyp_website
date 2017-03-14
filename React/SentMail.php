f04745e75e87075630db168e0b6a6f7f18d4f3b8
aiman <aiman@aiman-X550LD>
2017-03-14 12:43:27 +0800

    refactor view

--- React/SentMail.php
+++ React/SentMail.php
@@ -8,15 +8,15 @@ $mail = new PHPMailer;
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.mailgun.org';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'user@example.com';                 // SMTP username
-$mail->Password = 'secret';                           // SMTP password
+$mail->Username = 'postmaster@ameerulariff-king961.rhcloud.com';                 // SMTP username
+$mail->Password = 'ace9916edb7266d7e67382385ba873b6 ';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
 $mail->setFrom('from@example.com', 'Mailer');
-$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+$mail->addAddress('Aimank96@gmail.com', 'Joe User');     // Add a recipient
 $mail->addAddress('ellen@example.com');               // Name is optional
 $mail->addReplyTo('info@example.com', 'Information');
 $mail->addCC('cc@example.com');
