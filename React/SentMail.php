1d00e9597b6736abc5adeb9f95ad7d3915e405f0
aiman <aiman@aiman-X550LD>
2017-03-14 14:43:44 +0800

    refactor view

--- React/SentMail.php
+++ React/SentMail.php
@@ -1,12 +1,12 @@
 <?php
-require 'PHPMailer-master/PHPMailerAutoload.php';
+require 'PHPMailer-master/PHPMailerAutoload.php ';
 
 $mail = new PHPMailer;
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mailgun.org';  // Specify main and backup SMTP servers
+$mail->Host = 'v=spf1 include:mailgun.org ~all';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'postmaster@ameerulariff-king961.rhcloud.com';                 // SMTP username
 $mail->Password = 'ace9916edb7266d7e67382385ba873b6 ';                           // SMTP password
@@ -14,14 +14,12 @@ $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, 
 $mail->Port = 587;                                    // TCP port to connect to
 
 $mail->setFrom('from@example.com', 'Mailer');
-$mail->addAddress('Aimank96@gmail.com', 'Joe User');     // Add a recipient
-$mail->addAddress('ellen@example.com');               // Name is optional
+$mail->addAddress('Aimank96@gmail.com', 'Joe User');     // Add a recipient            // Name is optional
 $mail->addReplyTo('info@example.com', 'Information');
 $mail->addCC('cc@example.com');
 $mail->addBCC('bcc@example.com');
 
-$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+ // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = 'Here is the subject';
