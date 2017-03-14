71e56f48f177d54d63658f52f54394a3aa33c190
aiman <aiman@aiman-X550LD>
2017-03-14 12:25:00 +0800

    refactor view

--- React/SentMail.php
+++ React/SentMail.php
@@ -1,10 +1,12 @@
-<?php
-$to = "Aiman_k96@yahoo.com";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: webmaster@example.com" . "\r\n" .
-"CC: Aiman_k96@yahoo.com";
 
-mail($to,$subject,$txt,$headers);
-echo $subject;
+<?php
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("Aiman_k96@gmail.com.com","My subject",$msg);
+echo $msg;
 ?>
