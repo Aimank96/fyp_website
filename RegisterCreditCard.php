4bab9a972e4cdd285568d56ccfdbb1862fa490b7
aiman <aiman@aiman-X550LD>
2017-03-16 22:32:59 +0800

    Merge origin/master
    Conflicts:
    	index.php

---
+++ RegisterCreditCard.php
@@ -0,0 +1,20 @@
+<?php
+include 'DBCONFIG.php';
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    
+    $CardNumber = $_POST['CardNumb'];
+    $Expiry = $_POST['Expiry'];
+    $CVV = $_POST['CVV'];
+    
+    echo "uyyt";
+    
+    
+}
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
