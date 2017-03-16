7dab7f7ae7c6fc73c792c43c4ec111dd5569989c
aiman <aiman@aiman-X550LD>
2017-03-16 22:46:06 +0800

    Merge origin/master
    Conflicts:
    	index.php

--- RegisterCreditCard.php
+++ RegisterCreditCard.php
@@ -11,7 +11,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     
     
 }
-    echo "uyyt";
+    echo "hye";
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
