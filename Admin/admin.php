b775b4892f471a646cf220581ccec274d6a96b5e
Meerul <Meerul@192.168.0.86>
2017-03-29 23:20:53 +0800

    Merge origin/master
    Conflicts:
    	index.php
    	nbproject/project.properties
    	nbproject/project.xml

--- Admin/admin.php
+++ Admin/admin.php
@@ -6,23 +6,9 @@
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
-<link href="default.css" rel="stylesheet" type="text/css" media="all" />
-<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
-<style type="text/css">
-#page #main #welcome .title p {
-	font-size: 9px;
-}
-#page #main #welcome .title p {
-	font-size: 14px;
-}
-#page #main #welcome .title p {
-	font-size: 18px;
-}
-#page #main #welcome .title p {
-	font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
-}
-</style>
+<link href="asset/CSS/default.css" rel="stylesheet" type="text/css"/>
 
+<link href="asset/CSS/fonts.css" rel="stylesheet" type="text/css"/>
 <!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
 
 </head>
@@ -30,35 +16,79 @@
 <div id="page" class="container">
 	<div id="header">
 		<div id="logo">
-			<img src="images.jpg" alt="" />
+                    <img src="asset/Image/icon.jpeg" alt=""/>
 			<h1><a href="http://127.0.0.1/web/Home.html">Book Lover</a></h1>
 			
 		</div>
 		<div id="menu">
 			<ul>
-				<li class="current_page_item"><a href="http://127.0.0.1/web/Home.html" accesskey="1" title="">Homepage</a></li>
-				<li><a href="http://127.0.0.1/web/AboutUs.html"  title="">Admin</a></li>
-                                <li><a href="http://127.0.0.1/web/AboutUs.html"  title="">Admin</a></li>
-				
-				
-				
+				 <li  ><a href="home.php"  title="">HOME</a></li>
+                           
+                            <li  class="current_page_item"> <a href="admin.php"  title="">ADMIN</a></li>
 			</ul>
 		</div>
 	</div>
 	<div id="main">
 		<div id="banner">
-			<img src="book-love.jpg" alt="" width="103%" height="384" class="image-full" />
+                    <img class="image-full"  src="asset/Image/WhatsApp Image 2017-03-29 at 9.59.03 PM.jpeg" alt=""/>
 		</div>
 		<div id="welcome">
-		  <div class="title">
-			<h2></h2>
-	        <p>&quot;A library is like the best person you know - politely quiet but endlessly informative, amazingly knowledgeable but charming discreet, open to everyone yet subject to your every whim, and, sadly, flammable if one isnt't careful. Be careful with yours.&quot; <a href="#" class="button">Eti&quot;am posuere</a></p>
-		  </div>
-		  -
-		  Lemony Snicket
+			<div class="title">
+				<h2></h2>
+				Admin :</div>
+				
+<html>
+<head>
+<? ob_start(); ?>
+</head>
+
+<body>
+
+<form action="autoLogin.php" method="post">
+<table align="center">
+	<tr>
+	<td>Username</td>
+	<td>:</td>
+	<td><input name="myusername" type="text"/></td>
+	</tr>
+	<tr>
+	<td>Password</td>
+	<td>:</td>
+	<td><input name="mypassword" type="password"/></td>
+	</tr>
+	<tr>
+	<td>&nbsp;</td>
+	<td>&nbsp;</td>
+	<td><input name="submit" type="submit" value="Login" /></td>
+	</tr>
+	</table>
+</form>
+
+</body> <?php ?>
+</html>
+<div align="center"></div>
+	
+				
+				
+			<ul class="actions">
+				<li><a href="#" class="button">Etiam posuere</a></li>
+			</ul>
 		</div>
 	</div>
-    <?php ?>
 </div>
 </body>
 </html>
+		      
+
+
+
+			</div>
+			<ul class="actions">
+				<li><a href="#" class="button">Etiam posuere</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+</body>
+</html>
+
---
+++ Admin/home.php
@@ -0,0 +1,66 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+<link href="asset/CSS/default.css" rel="stylesheet" type="text/css"/>
+<link href="asset/CSS/fonts.css" rel="stylesheet" type="text/css"/>
+<style type="text/css">
+#page #main #welcome .title p {
+	font-size: 9px;
+}
+#page #main #welcome .title p {
+	font-size: 14px;
+}
+#page #main #welcome .title p {
+	font-size: 18px;
+}
+#page #main #welcome .title p {
+	font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+}
+</style>
+
+<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+</head>
+<body>
+<div id="page" class="container">
+	<div id="header">
+		<div id="logo">
+			
+                        <img src="asset/Image/icon.jpeg" alt=""/>
+			<h1><a href="http://127.0.0.1/web/Home.html">Rapid KL</a></h1>
+			
+		</div>
+		<div id="menu">
+			<ul>
+                            <li class="current_page_item" ><a href="home.php"  title="">HOME</a></li>
+                           
+                            <li> <a href="admin.php"  title="">ADMIN</a></li>
+				
+				
+				
+			</ul>
+		</div>
+	</div>
+	<div id="main">
+		<div id="banner">
+                    <img class="image-full" src="asset/Image/homepage.jpeg" alt=""  width="1500" height="650" />
+		</div>
+		<div id="welcome">
+		  <div class="title">
+			<h2></h2>
+	        <p>"We operate one of the largest public transport networks in Malaysia.Our transport services are integrated,
+                    giving ease to changing between them when commuting in Greater Klang Valley, Penang, Kamunting and Kuantan
+                    . Connecting to the city center business districts and popular sites is now made easier.</p>
+		  </div>
+		 
+		</div>
+	</div>
+    <?php ?>
+</div>
+</body>
+</html>
