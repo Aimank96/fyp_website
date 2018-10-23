7f8f3a42f66880eb641a89fa9793be598cbbe772
aiman <Aimank96@gmail.com>
2018-10-23 22:16:30 +0800

    asdasdas
    asdasd

--- DBCONFIG.php
+++ DBCONFIG.php
@@ -1,7 +1,14 @@
 <?php
 
 echo 'selamat datang';
-// $url = getenv('JAWSDB_URL');
+
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
 // $dbparts = parse_url($url);
 //
 // // $hostname = $dbparts['host'];
