89ab109f0d47390edbdd760913ff505d7432f0a6
aiman <Aimank96@gmail.com>
2018-10-23 22:18:08 +0800

    sdff

--- DBCONFIG.php
+++ DBCONFIG.php
@@ -9,6 +9,15 @@ $hostname = $dbparts['host'];
 $username = $dbparts['user'];
 $password = $dbparts['pass'];
 $database = ltrim($dbparts['path'],'/');
+
+
+$conn = new mysqli($hostname, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connection was successfully established!";
 // $dbparts = parse_url($url);
 //
 // // $hostname = $dbparts['host'];
