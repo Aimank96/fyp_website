6529e95d243e7b68a3816f0656939cd456116e5c
aiman <Aimank96@gmail.com>
2018-10-23 22:14:54 +0800

    sssss

--- DBCONFIG.php
+++ DBCONFIG.php
@@ -1,31 +1,21 @@
 <?php
 
 echo 'selamat datang';
-$url = getenv('JAWSDB_URL');
-$dbparts = parse_url($url);
-
-// $hostname = $dbparts['host'];
-// $username = $dbparts['user'];
-// $password = $dbparts['pass'];
-$database = ltrim($dbparts['path'],'/');
-
-$dbhost = $dbparts['host'];
-$dbport = "3306"
-$dbuser = $dbparts['user'];
-$dbpwd = $dbparts['pass'];
-
-if($con = mysqli_connect($dbhost,$dbuser,$dbpwd,$database,$dbport)){
-     echo 'openshift berjaya';
-}
-//  else{
-//     $dbhost ='localhost';
-// $dbuser ='root';
-// $dbpwd = 'root';
-// if($con = mysqli_connect($dbhost,$dbuser,$dbpwd,"ameerulariff",$dbport)){
-//     echo 'berjaya';
+// $url = getenv('JAWSDB_URL');
+// $dbparts = parse_url($url);
+//
+// // $hostname = $dbparts['host'];
+// // $username = $dbparts['user'];
+// // $password = $dbparts['pass'];
+// $database = ltrim($dbparts['path'],'/');
+//
+// $dbhost = $dbparts['host'];
+// $dbport = "3306"
+// $dbuser = $dbparts['user'];
+// $dbpwd = $dbparts['pass'];
+//
+// if($con = mysqli_connect($dbhost,$dbuser,$dbpwd,$database,$dbport)){
+//      echo 'openshift berjaya';
 // }
-//  else {
-//    echo 'gagal';
-// }
-// }
+
 ?>
